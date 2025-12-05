3f28c3772272f730f5b9bfb35b948a34e7614a9a
Hendrix Roa <hroa@univision.net>
2025-12-05 12:18:36 -0500

    refactor: add type hints to NoteController

--- src/core/features/note/note_controller.py
+++ src/core/features/note/note_controller.py
@@ -1,9 +1,9 @@
 from .note import Note
+from .note_repository import NoteRepository
 
 class NoteController:
 
-
-    def __init__(self, note_repository):
+    def __init__(self, note_repository: NoteRepository):
         self.note_repository = note_repository
 
     async def get_notes(self) -> list[Note]:
